1e8b51ce412440fb11ff3f118a755575412512f9
bautirenaudo <78118334+bautirenaudo@users.noreply.github.com>
2022-03-02 01:49:48 -0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -6,25 +6,29 @@
     const daysConatiners = document.querySelectorAll(".day");
     const fragment = document.createDocumentFragment();
     let date = new Date(); let newDate = date.toLocaleString("en-ES", { timeZone: "America/Buenos_Aires" });
-    newDate = newDate.split(",").splice(0, 1).toString().split("/"); getDay = date.getDay();
+    newDate = newDate.split(",").splice(0, 1).toString().split("/");
+    getDay = date.getDay();
 
-    let arrayDays = ["", "Lun", "Ma", "Mie", "Jue", "Vie", "Sab", "Dom",]
+    let arrayDays = ["Dom", "Lun", "Ma", "Mie", "Jue", "Vie", "Sab"]
 
     daysConatiners.forEach((e, i) => {
-        //console.log(date);         
-        //console.log(newDate);   
-
         const li = document.createElement("li");
         const span = document.createElement("b");
 
         span.textContent = newDate[1] + "/" + newDate[0];
         li.textContent = arrayDays[getDay] + " ";
+
+        const liId = arrayDays[getDay];
+        const spanId = newDate[1] + "/" + newDate[0];
+
+        li.id = liId;
+        span.id = spanId;
         li.append(span); document.querySelector(".menu").append(li);
 
         date.setDate(date.getDate() + 1);
         newDate = date.toLocaleString("en-ES", { timeZone: "America/Buenos_Aires" });
         newDate = newDate.split(",").splice(0, 1).toString().split("/");
-        getDay = date.getDate();
+        getDay = date.getDay();
     })
 
 
@@ -41,25 +45,49 @@
 
     //extras
     document.querySelectorAll(".menu li").forEach(li => {
-        li.addEventListener("click", () => {
-            document.querySelector(".conteiner-turnos").style.cssText = "transform: translateX(0%);";
+        li.addEventListener("click", (e) => {
+            e = e.target.closest("li")
+            const dateSelected = e.id;
+            const fecha = e.querySelector("b").id;
+            document.querySelector(".dia-seleccionado").textContent = dateSelected + " " + fecha;
 
+
+            document.querySelector(".conteiner-turnos").style.cssText = "transform: translateX(0%);";
             document.querySelector(".conteiner-home").style.cssText = "transform: translateX(-100%);";
         })
     })
 
     document.querySelector(".back-button").addEventListener("click", () => {
         document.querySelector(".conteiner-home").style.cssText = "transform: translateX(0%);";
-
         document.querySelector(".conteiner-turnos").style.cssText = "transform: translateX(100%);";
+    })
+
+    window.addEventListener("click",(e)=>{
+        if(e.target.classList.contains("seleccionar")){
+            document.querySelector(".conteiner-home").style.cssText = "transform: translateX(0%);";
+            document.querySelector(".conteiner-turnos").style.display = "none";
+            document.querySelector(".conteiner-con-reserva").style.display = "block";
+        }
     })
 
 
 
     //crear turnos
-    const select = document.createElement("select");
     for (let i = 0, j = 0, k = 0; i < 40; i++) {
-        const option = document.createElement("option");
+        if (j == 45 || j == 15) {
+            if (j == 45) {
+                j = 0
+            }
+            else {
+                j += 15
+            }
+            continue;
+        }
+        const li = document.createElement("li");
+        const turno = document.createElement("div");
+        const seleccionarElement = document.createElement("p");
+        seleccionarElement.textContent = "SELECCIONAR";
+        seleccionarElement.classList.toggle("seleccionar");
 
         if (i % 4 == 0) {
             hour = 10 + k;
@@ -72,10 +100,11 @@
             minute += "0"
         }
 
-        const turno = hour + ":" + minute;
-        option.textContent = turno;
+        turno.textContent = hour + ":" + minute;
+        li.append(turno)
+        li.append(seleccionarElement)
 
-        select.append(option)
+        fragment.append(li);
 
         if (j == 45) {
             j = 0
@@ -84,6 +113,7 @@
             j += 15
         }
     }
+    document.querySelector(".turnos-caja").append(fragment);
     //
 
 
